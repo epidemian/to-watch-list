0261e20b7aaf95e9f401e431a41d6c22d9e3c7ae
Demian Ferreiro <epidemian@gmail.com>
2012-01-30 14:56:27 -0300

    A little code cleanup

--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ require 'json'
 require 'open-uri'
 require 'nokogiri'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'mysql://localhost/sinatra_development')
+database_url = ENV['DATABASE_URL'] || 'mysql://localhost/sinatra_development'
+DataMapper.setup :default, database_url
 
 class ToWatch
   include DataMapper::Resource
@@ -19,12 +20,6 @@ class ToWatch
 end
 
 DataMapper.auto_upgrade!
-
-# Uncomment for some bootstrap data
-#DataMapper.auto_migrate!
-#Video.create :title => "RickRoll'D", :comment => 'For the lulz', :youtube_id => 'oHg5SJYRHA0'
-#Video.create :title => "Nyan Cat [original]", :youtube_id => 'QH2-TGUlwu4'
-#Video.create :title => "Another rick roll...", :comment => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', :youtube_id => 'oHg5SJYRHA0'
 
 set :default_encoding => "utf-8"
 
@@ -44,10 +39,12 @@ end
 
 # REST methods for ToWatch resource.
 post '/towatch', :provides => :json do
-  json = JSON.parse(request.body.read)
-  tw = ToWatch.new(json)
+  req  = request.body.read
+  json = JSON.parse req
+  tw   = ToWatch.new json
+
   tw.title = get_title_for_link tw.link if tw.title.nil?
-  tw.save()
+  tw.save
 
   return 400, tw.errors.to_hash.to_json unless tw.valid?
   tw.to_json
@@ -62,9 +59,10 @@ get '/towatch/:id', :provides => :json do
 end
 
 put '/towatch/:id', :provides => :json do
-  id = params[:id].to_i
-  tw = ToWatch.get(id)
-  json = JSON.parse(request.body.read)
+  id   = params[:id].to_i
+  tw   = ToWatch.get id
+  text = request.body.read
+  json = JSON.parse text
 
   return 404 if tw.nil?
 
@@ -75,7 +73,7 @@ end
 
 delete '/towatch/:id' do
   id = params[:id].to_i
-  tw = ToWatch.get(id)
+  tw = ToWatch.get id
   return 404 if tw.nil?
   tw.destroy
 end
@@ -83,11 +81,14 @@ end
 # Return a convenient title for a link.
 def get_title_for_link link
   begin
-    doc = Nokogiri::HTML(open(link))
-    node = doc.search('h1').first  || doc.search('title').first
+    doc  = Nokogiri::HTML open(link)
+    node = doc.search('h1').first || doc.search('title').first
     node.xpath('.//text()').to_s.strip
   rescue
-    # If something goes wrong, use link as a title.
+    # If something goes wrong (e.g. "link" is not a valid URL and "open" 
+    # complained, or the resource is not HTML and Nokogiri shat the bed, or
+    # the HTML had no 'h1' not 'title' tags, so "node" was nil, etc), use the 
+    # link as a title :)
     link
   end
 end
